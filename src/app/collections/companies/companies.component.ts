8c2a76a31a0f8ff4b5d31e47e8267beeb791173f
VIVEK <Vivek.17beceg105@gmail.com>
2022-07-04 17:50:01 +0530

    Impliment Tabs Menu

---
+++ src/app/collections/companies/companies.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-companies',
+  templateUrl: './companies.component.html',
+  styleUrls: ['./companies.component.css'],
+})
+export class COmpaniesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
