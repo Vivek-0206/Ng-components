bb8d23ab904c826767586a368075e54037ddef03
VIVEK <Vivek.17beceg105@gmail.com>
2022-07-04 17:10:52 +0530

    Added Table Component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: '25', job: 'Programmer' },
+    { name: 'Jill', age: '52', job: 'Doctor' },
+    { name: 'John', age: '27', job: 'Teacher' },
+  ];
+
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
